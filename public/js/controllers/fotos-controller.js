51c9ef46aff59d07fad7a7a01cd689daf48fe152
marcelohmdias <marcelohmdias@gmail.com>
2015-08-31 02:13:09 -0300

    Iniciando projeto do curso.

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,9 @@
+(function() {
+  angular.module('alurapic')
+    .controller('FotosController', function ($scope) {
+      $scope.foto = {
+        'titulo': 'Leão',
+        'url': 'http://www.fundosanimais.com/Minis/leoes.jpg'
+      };
+    });
+})();
